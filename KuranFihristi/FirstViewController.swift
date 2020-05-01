e62e53e29f27130796bd21c5253ab8e8be4cdfbe
Cavad Heybetzade <heybetzadec@gmail.com>
2020-05-01 19:35:27 +0400

    Initial Commit

---
+++ KuranFihristi/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  KuranFihristi
+//
+//  Created by Cavad Heybetzade on 5/1/20.
+//  Copyright © 2020 Cavad Heybetzade. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KuranFihristiTests/KuranFihristiTests.swift
@@ -0,0 +1,34 @@
+//
+//  KuranFihristiTests.swift
+//  KuranFihristiTests
+//
+//  Created by Cavad Heybetzade on 5/1/20.
+//  Copyright © 2020 Cavad Heybetzade. All rights reserved.
+//
+
+import XCTest
+@testable import KuranFihristi
+
+class KuranFihristiTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
